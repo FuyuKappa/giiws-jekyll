46d09cd2caa732d6f13d79a58bbe61095794a032
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-05-25 19:48:17 +0800

    Added code for 'until banner starts' timer

--- static/js/indexFunctions.js
+++ static/js/indexFunctions.js
@@ -3,13 +3,23 @@ links.forEach(function(link){
 	link.innerHTML = link.innerHTML.replace("_",".")
 	
 	let end = link.querySelector("#runTimeData").textContent.split("- ")[1];
-	let elapsedDays = Math.round( (Date.now() - Date.parse(end)) / (1000*3600*24));
-	if(elapsedDays < 0){
-		link.querySelector("#daysSince").innerHTML = link.querySelector("#daysSince")
-													.innerHTML.replace("since banner ended","until banner ends");
-		elapsedDays = elapsedDays * -1;
+	if(end !== "??"){
+		console.log("end is ??");
+		let elapsedDays = Math.round( (Date.now() - Date.parse(end)) / (1000*3600*24));
+		if(elapsedDays < 0){
+			link.querySelector("#daysSince").innerHTML = link.querySelector("#daysSince")
+														.innerHTML.replace("since banner ended","until banner ends");
+			elapsedDays = elapsedDays * -1;
+		}
+		link.querySelector("#daysSinceData").innerHTML = elapsedDays;
 	}
-	link.querySelector("#daysSinceData").innerHTML = elapsedDays;
+	else{
+		let start = link.querySelector("#runTimeData").textContent.split("- ")[0];;
+		link.querySelector("#daysSince").innerHTML = link.querySelector("#daysSince")
+														.innerHTML.replace("since banner ended","until banner starts");
+		link.querySelector("#daysSinceData").innerHTML =  Math.round( (Date.now() - Date.parse(start)) / (1000*3600*24)) * -1;
+	}
+	
 	
 	/*
 	let numbers = link.innerHTML.split(".")
