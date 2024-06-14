f3fed002af96e5b6c10d47f852308aa384d44880
FuyuKappa <FuyuFuyuKoppe@gmail.com>
2024-06-14 21:10:49 +0800

    Added function to get days since last run for character in the modal. Gave class names to modal specific time info for ease of access

--- static/js/characterModal.js
+++ static/js/characterModal.js
@@ -2,9 +2,12 @@ let charPortraits = document.querySelectorAll(".charImg");
 charPortraits.forEach(function(charPortrait){
 	charPortrait.addEventListener('click', function(e){
 		e.preventDefault();
-		console.log("clicked " + charPortrait.getAttribute("data"));
 		populateData(charPortrait.getAttribute("data"));
-		renderModal(charPortrait.getAttribute("data"));
+		
+		let modalCharPatch = document.querySelector(".characterPatch");
+		modalCharPatch.innerText =  modalCharPatch.innerText.replace(/x/g, getDaysSinceLastRun());
+		
+		renderModal();
 	});
 });
 
@@ -15,7 +18,7 @@ closeButton.addEventListener('click', function(){
 
 //add check if we are in mobile mode?
 
-function renderModal(character){
+function renderModal(){
 	let blur = document.querySelector(".globalBlur");
 	blur.style.display = "block";
 	blur.style.pointerEvents = "auto";
@@ -58,15 +61,17 @@ function populateData(charName){
 	modalCharName.innerText = charName;
 	
 	let modalCharPatch = document.querySelector(".characterPatch");
-	if(!(banners.length === 1 && currentVersion === banners[0].version)) //if the current patch is the character's only run
-		modalCharPatch.innerText = "Last run: V" + (currentVersion === banners[0].version ? banners[1].version : banners[0].version) + " (x days ago)";
-	else
+	if(banners.length === 1 && currentVersion === banners[0].version) //if the current patch is the character's only run
 		modalCharPatch.innerText = "This is their first run";
+	else if(currentVersion === banners[0].version)
+		modalCharPatch.innerText = "Last run: V" + banners[1].version + " (x days ago)";
+	else
+		modalCharPatch.innerText = "Last run: V" + banners[0].version + " (x days ago)";
 	
 	let modalCharRuns = document.querySelector(".characterRuns");
 	modalCharRuns.innerText = "Total amount of runs: " + banners.length; //check if the character is currently running; if so, -1.
-	//Days since run?
 	
+	//remove all left over banners for new data
 	let existingBanner = document.querySelectorAll(".banner");
 	if(existingBanner.length > 0){
 		existingBanner.forEach(function(banner){
@@ -119,7 +124,7 @@ function populateData(charName){
 		runTime.innerHTML = "Banner duration:<br><span id='runTimeData'><date value=" + datesData[key].start + ">--</date> - <date value=" + datesData[key].end + ">--</date></span>"
 		
 		let daysSince = document.createElement("div");
-		daysSince.className = "timeInfoData";
+		daysSince.className = "timeInfoData modalDays";
 		daysSince.id = "daysSince";
 		daysSince.innerHTML = "Days since banner ended:<br><span id='daysSinceData'>--</span>";
 		
@@ -135,10 +140,22 @@ function populateData(charName){
 		
 		newBanner.appendChild(center);
 		
-		bannerList.appendChild(newBanner);
+		
+		bannerList.appendChild(document.createElement("div").appendChild(newBanner));
 		
 		calculateTime();
 		parseDates();
 	});
+}
+
+function getDaysSinceLastRun(){
+	let modalDays = document.querySelectorAll(".modalDays");
+	if(modalDays[0].innerHTML.split(" ")[0] === "Time")
+		if(modalDays.length === 1)
+			return "In current version";
+		else
+			return modalDays[1].querySelector("#daysSinceData").innerText
+	else
+		return modalDays[0].querySelector("#daysSinceData").innerText
 }
 
